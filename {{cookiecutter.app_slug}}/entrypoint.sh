20e8090aaa70e31867957756b20c86efb86cf15e
Robert Sunčan Pagon <robert.pagon@gmail.com>
2024-08-14 21:16:36 +0200

    Sitnice

--- {{cookiecutter.app_slug}}/entrypoint.sh
+++ {{cookiecutter.app_slug}}/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Ovo koristi docker:
+#    grep entrypoint -R *
+#      Dockerfile:# add entrypoint.sh
+#      Dockerfile:COPY ./entrypoint.sh /usr/src/app/entrypoint.sh
+#      Dockerfile:CMD ["./entrypoint.sh"]
+#      requirements_new.txt:entrypoints==0.3
+
 echo "Waiting for postgres..."
 
 while ! nc -z web-db 5432; do
--- {{cookiecutter.app_slug}}/project/server/models.py
+++ {{cookiecutter.app_slug}}/project/server/models.py
@@ -13,6 +13,7 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
